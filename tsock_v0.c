6a509f7865fff63c9402995ba9779ad9dfbda284
Merlin <91677699+GrandChacalDominant@users.noreply.github.com>
2022-02-17 08:04:41 +0100

    Add files via upload

--- tsock_v0.c
+++ tsock_v0.c
@@ -21,13 +21,13 @@ données du réseau */
 void main (int argc, char **argv)
 {
 	int c;
-	int protocol = 0;
+	int protocol=0;
 	extern char *optarg;
 	extern int optind;
-	int lg_mesg_emis =30;
+	int lg = 30;
 	int port = atoi(argv[argc-1]);
 	port = htons(port);
-	char adr = (argv[argc-2]);
+	char *adr = (argv[argc-2]);
 	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
 	int source = -1 ; /* 0=puits, 1=source */
 	while ((c = getopt(argc, argv, "pn:su")) != -1) {
@@ -52,11 +52,10 @@ void main (int argc, char **argv)
 			nb_message = atoi(optarg);
 			break;
 
-        
-        case 'u': 
-            protocol = 1;
-            break;
-            
+		case 'u':
+			protocol=1;
+			break;
+
 		default:
 			printf("usage: cmd [-p|-s][-n ##]\n");
 			break;
@@ -86,114 +85,40 @@ void main (int argc, char **argv)
 		printf("nb de tampons à envoyer = infini\n");
 
 	}
-	if (protocol=1) {
-	    if (source == 1) {
-	    	struct hostent *hp ;
-			struct sockaddr_in adr_distant;
-			/* main */
-			...
-			/* affectation domaine et n° de port */
-			/* reset de la structure */
-			memset((char *)& adr_distant, 0, sizeof(adr_distant)) ;
-			adr_distant.sin_family = AF_INET ; /* domaine Internet*/
-			adr_distant.sin_port = port ; /* n° de port */
-			/* affectation @_IP */
-			if ((hp = gethostbyname(adr)) = = NULL)
-			{ 
-				printf("erreur gethostbyname\n") ;
-				exit(1) ; 
-			}
-			memcpy((char*)&(adr_distant.sin_addr.s_addr),hp->h_addr,hp->h_length);
-
-
-			/* association @ socket représentation interne */
-		
-
-			for (int nb_mess=0;nb_mess<nb_message;nb_mess++)
-			{
-				char * message;
-				message = malloc (lg);
-				char motif = (nb_mess+1)%26;
-				construire_message(message,motif,lg,nb_mess);
-			}
-
-			if (sendto(sock,message,lg,0, (struct sockaddr *)&adr_local, lg_adr_local) ==-1)
-			{
-				printf("échec d'envoi du message\n") ;
-				exit(1) ; 
-			}
-
-
-	    	if (close(sock) == -1)
-			{ 
-				printf("échec de destruction du socket\n") ;
-				exit(1) ; 
-			}
-	    }
-	    else if (source == 0) {
-
-			int sock ; /* représentation interne du socket local */
-			struct sockaddr_in adr_local ; /* adresse du socket local */
-			int lg_adr_local = sizeof(adr_local) ;
-
-			/* création du socket */
-			if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-			{ 
-				printf("échec de création du socket\n") ;
-				exit(1) ; 
-			}
-			/* construction de l’@ du socket avec n° de port = port et */
-			/* @ IP = celle de la machine sur laquelle s'exécute le proc */
-			memset((char*)&adr_local, 0, sizeof(adr_local)) ; /* reset */
-			adr_local.sin_family = AF_INET ;
-			adr_local.sin_port = port;
-			adr_local.sin_addr.s_addr = adr ;
-
-			if (bind (sock, (struct sockaddr *)&adr_local, lg_adr_local) == -1)
-			{ 
-				printf("échec du bind\n") ;
-				exit(1) ;
-			}
-			if recvfrom
-	    }
-	}
-	void construire_message(char *message, char motif, int lg, int nb_mess) {
-		
-        for (int i=nb_mess-1;i<lg;i++) 
-        {
-            message[i] = motif;
-        }
-		n=nb_mess;
-		for (int i=4;i>=0;i--)
+	void construire_message(char *message, char motif, int lg, int nb_message) 
+	{
+		int i;
+		int nb=nb_message;
+		int fact = 10000;
+		for (i=0;i<lg;i++) 
 		{
-			if (n!=0)
+			if (i>4)
 			{
-				message[i]=n%nb_digit(n);
-				n=n/10^(nb_digit(n));
+				message[i] = motif;
 			}
 			else 
 			{
-				message[i]=-;
+				if (nb/fact==0)
+				{
+					message[i]='-';
+				}
+				else
+				{
+					message[i]=nb/fact;
+					nb=nb%fact;
+				}
+				fact=fact/10;
 			}
-		} 
-		
-    }
-    void afficher_message(char *message, int lg) {
-        int i;
-        printf("message construit : ");
-        for (i=0;i<lg;i++) {
-
-            printf("%c", message[i]); printf("\n");
-        }
-    }
-	int nb_digit(int n)
+		}
+	}
+	void afficher_message(char *message, int lg) 
 	{
-		int count=0;
-		do {
-			n /= 10;
-			++count;
-		}while (n!=0);
-		return count-1;
+		int i;
+		printf("message construit : ");
+		for (i=0;i<lg;i++) 
+		{
+			printf("%c", message[i]); printf("\n");
+		}
 	}
 
 }
